d65585a379cf8c7681fb6de558ba1a77ee2dbaa2
tailgo <zenosturtle@outlook.com>
2017-04-14 23:47:45 +0800

    !

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,18 @@
 (function () {
 
+  var App = WinJS.Application;
+
+  var nav = window.nav = {
+    toblog: WinJS.UI.eventHandler(function () {
+      console.log('toblog');
+    })
+  }
+
   WinJS.UI.processAll().done(function () {
     var splitView = document.querySelector('.split-view').winControl;
     new WinJS.UI._WinKeyboard(splitView.paneElement); // Temporary workaround: Draw keyboard focus visuals on NavBarCommands
   });
+
+  App.start();
 
 })();
