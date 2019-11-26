7a8cff5c0bda1e51e0b17ad8a059558c4f92497f
tailgo <zenosturtle@outlook.com>
2019-11-27 01:00:58 +0800

    test build

---
+++ precache-manifest.d81488a6c622348acd20bcf8d558c06c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c492c470d6f89bde09dc1a6b1a2cca1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b869d8e3057aca6e3628",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e93e154982ca7c359623",
+    "url": "/static/js/2.c5af0d44.chunk.js"
+  },
+  {
+    "revision": "b869d8e3057aca6e3628",
+    "url": "/static/js/main.d7218125.chunk.js"
+  },
+  {
+    "revision": "75f40d14e8416d0613a7",
+    "url": "/static/js/runtime-main.46486d53.js"
+  }
+]);
